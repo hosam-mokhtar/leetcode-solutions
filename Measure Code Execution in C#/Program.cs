e91a6b933478576b598a5e2cfaf2078d465304b4
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-02-12 03:49:08 +0200

    Measure Code Execution Time on Your Device in C#

---
+++ Measure Code Execution in C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Measure_Code_Execution_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            // Code block to be measured
+
+
+            timer.Stop();
+            TimeSpan elapsedTime = timer.Elapsed;
+
+            Console.WriteLine($"{elapsedTime.Seconds} s");
+            Console.WriteLine($"{elapsedTime.TotalMilliseconds} ms");
+        }
+    }
+}
