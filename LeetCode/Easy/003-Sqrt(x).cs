79389830718d9f86bb07639b0a5bb3f584302389
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-03-06 09:02:31 +0200

    Refactor sqrt(x) implementation using binary search

--- LeetCode/Easy/003-Sqrt(x).cs
+++ LeetCode/Easy/003-Sqrt(x).cs
@@ -8,7 +8,21 @@ Language: C#
 public class Solution {
     public int MySqrt(int x)
  {
-     int y= (int)Math.Sqrt(x);
-     return y;
+            if (x == 0 || x == 1) return x;
+            int left = 1;
+            int right = x;
+            while (left <= right) {
+                int mid = (left+right) / 2;
+                if (mid == x / mid)
+                    return mid;
+                else if (x / mid > mid)
+                    left = mid + 1;
+                else
+                    right = mid - 1;    
+            }
+                return right;    // right is the floor of the square root
+     // return (int)Math.Sqrt(x);
+     // or
+     // return (int)Math.Pow(x,0.5);   
  }
 }
