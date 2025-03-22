31c5620f3bc298963fb24f6a63097d6e03953bc0
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-03-22 16:47:51 +0200

    Update Two Sum

--- LeetCode/Easy/001-Two Sum.cs
+++ LeetCode/Easy/001-Two Sum.cs
@@ -4,21 +4,17 @@ Language: C#
 
 
 
-public class Solution {  
-        public int[] TwoSum(int[] nums, int target)
-        {
-            Dictionary<int, int> dict = new ();
-            for (int i = 0; i < nums.Length; i++)
-            {
-                int a = nums[i];
-                int b = target - a;
-                if (dict.ContainsKey(b))
-                    {
-                        return [dict[b],i];
-                    }
-                if (!dict.ContainsKey(a))
-                    dict.Add(a, i);
-            }
-            return [0];
-        }
+public class Solution {
+       public int[] TwoSum(int[] nums, int target)
+       {
+           Dictionary<int, int> dict = [];
+           dict[nums[0]] = 0;
+           for (var i = 1; i < nums.Length; i++)
+           {
+               var b = target - nums[i];
+               if (dict.TryGetValue(b, out int value)) return [value, i];
+               dict[nums[i]] = i;
+           }
+           return [0];
+           }
 }
