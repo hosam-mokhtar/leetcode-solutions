44e0bac052d1994165de2605e2f1336adff93bd8
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-07-17 05:01:21 +0300

    Two Sum II - Input Array Is Sorted

---
+++ LeetCode C#/Medium/020-Two Sum II - Input Array Is Sorted.cs
@@ -0,0 +1,27 @@
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+Language: C#
+
+
+
+
+
+
+
+public class Solution {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int index1 = 0, index2 = numbers.Length - 1;
+
+            while (index1 < index2)
+            {
+                if (numbers[index1] + numbers[index2] == target)
+                    return [index1 + 1, index2 + 1];
+                else if (numbers[index1] + numbers[index2] > target)
+                    index2--;
+                else
+                    index1++;
+            }
+
+            return [-1, -1];
+        }
+}
