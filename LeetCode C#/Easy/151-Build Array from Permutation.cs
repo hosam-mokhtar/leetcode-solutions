f72a84156ef02c8c32d5c97489c13de57aa3b04a
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-07-22 18:48:35 +0300

    Build Array from Permutation

---
+++ LeetCode C#/Easy/151-Build Array from Permutation.cs
@@ -0,0 +1,21 @@
+Link: https://leetcode.com/problems/build-array-from-permutation/
+Language: C#
+
+
+
+
+
+
+
+public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+                ans[i] = nums[nums[i]];
+
+            return ans;
+        }
+    }
