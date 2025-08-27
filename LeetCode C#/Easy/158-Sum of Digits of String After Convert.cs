c180d57e27c54144b208768684b8e209dcd9f08f
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-08-28 01:51:11 +0300

    Update Sum of Digits of String After Convert

--- LeetCode C#/Easy/158-Sum of Digits of String After Convert.cs
+++ LeetCode C#/Easy/158-Sum of Digits of String After Convert.cs
@@ -10,24 +10,30 @@ public class Solution {
         public int GetLucky(string s, int k)
         {
             int sum = 0;
-            string temp = "";
 
             for (int i = 0; i < s.Length; i++)
             {
-                temp += s[i] - 'a' + 1;
+                sum += Sum(s[i] - 'a' + 1);
             }
 
-            while (k >= 1)
+            while (k > 1)
             {
-                sum = 0;
-
-                for (int i = 0; i < temp.Length; i++)
-                {
-                    sum += temp[i] - '0';
-                }
-
-                temp = sum.ToString();
+                sum = Sum(sum);
                 k--;
+            }
+
+            return sum;
+        }
+
+        public int Sum(int n)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int digit = n % 10;
+                n /= 10;
+                sum += digit;
             }
 
             return sum;
