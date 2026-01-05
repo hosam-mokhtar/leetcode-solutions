3e501e01f4caa211aed5b1d9e1ca1b7d6827259a
Hossam <hossam.mokhtar.fcis@gmail.com>
2026-01-05 11:51:43 +0200

    Find Users with High Token Usage

---
+++ LeetCode MS SQL Server/Easy/040-Find Users with High Token Usage.sql
@@ -0,0 +1,13 @@
+Link: https://leetcode.com/problems/find-users-with-high-token-usage/
+Tool: MS SQL Server
+
+
+
+
+
+
+SELECT user_id, count(prompt) AS prompt_count, round(sum(tokens) * 1.00 / count(tokens), 2) AS avg_tokens
+FROM prompts
+GROUP BY user_id
+HAVING count(prompt) > 2 AND max(tokens) > avg(tokens) 
+ORDER BY avg_tokens DESC
