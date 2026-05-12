f9414b6756ced1ee72a8589373f7d009bc127036
Hossam <hossam.mokhtar.fcis@gmail.com>
2026-05-13 00:14:04 +0300

    Update

--- LeetCode C#/Easy/274-Check if Array Is Sorted and Rotated.cs
+++ LeetCode C#/Easy/274-Check if Array Is Sorted and Rotated.cs
@@ -7,40 +7,37 @@ Language: C#
 
 
 
-public class Solution {
-        public bool Check(int[] nums)
+public class Solution
+{
+    public bool Check(int[] nums)
+    {
+        if (nums.Length == 1) return true;
+
+        int[] temp = new int[nums.Length * 2];
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (nums.Length == 1) return true;
-            
-            int[] temp = new int[nums.Length * 2];
-            int index = 0;
+            temp[i] = nums[i];
+            temp[nums.Length + i] = nums[i];
+        }
 
-            for (int i = 0; i < temp.Length; i++)
+        int c = 1;
+        int maxLen = 1;
+
+        for (int i = 1; i < temp.Length; i++)
+        {
+            if (temp[i - 1] <= temp[i])
             {
-                if (index == nums.Length)
-                    index = 0;
-                temp[i] = nums[index];
-                index++;
-            }
-
-            int c = 1;
-            int maxLen = 1;
-
-            for (int i = 1; i < temp.Length; i++)
-            {
-                if (temp[i - 1] <= temp[i])
-                    c++;
-                else
-                {
-                    maxLen = Math.Max(maxLen, c);
-                    c = 1;
-                }
-
+                c++;
                 maxLen = Math.Max(maxLen, c);
+
                 if (maxLen == nums.Length)
                     return true;
             }
-
-            return false;
+            else
+                c = 1;
         }
+
+        return false;
+    }
 }
